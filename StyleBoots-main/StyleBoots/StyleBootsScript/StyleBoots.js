e77679140225988cb5204387f047f631729c254b
Ayano <148707134+Ayano-Oficial@users.noreply.github.com>
2023-11-01 16:50:01 -0300

    update

---
+++ StyleBoots-main/StyleBoots/StyleBootsScript/StyleBoots.js
@@ -0,0 +1,14 @@
+/*Tabela tabls001 */
+var tabls001Cor = document.getElementById("minha-tabls001");
+var linhaTabls001 = tabls001Cor.getElementsByTagName("td");
+
+for (var i = 0; i < linhaTabls001.length; i++) {
+    if(i % 2 === 1) {
+        linhaTabls001[i].classList.add('tabls-cor');
+    }
+};
+
+/*Fim Tabela tabls001 */
+
+/*######################################## */
+
