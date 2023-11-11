738e863c59f442faa32a1d52f797032a4c7298fd
Ayano <148707134+Ayano-Oficial@users.noreply.github.com>
2023-11-11 00:23:43 -0300

    atualização

--- script/scriptComponentesCopy.js
+++ script/scriptComponentesCopy.js
@@ -1,218 +1,266 @@
-const btnCopiar = document.getElementById("copiarMnu001");
-
-const codigo = document.getElementById("codigo")
-
-btnCopiar.addEventListener("click", function(){
-    const range = document.createRange()
-    range.selectNode(codigo)
-    window.getSelection().removeAllRanges()
-    window.getSelection().addRange(range)
-
-
-    document.execCommand("copy")
-
-    window.getSelection().removeAllRanges()
-
-    alert("Código copiado!")
-})    
-
-
-
-const btnCopiar2 = document.getElementById("copiarMnu002");
-
-const codigo2 = document.getElementById("codigo2")
-
-btnCopiar2.addEventListener("click", function(){
-    const range = document.createRange()
-    range.selectNode(codigo2)
-    window.getSelection().removeAllRanges()
-    window.getSelection().addRange(range)
-
-
-    document.execCommand("copy")
-
-    window.getSelection().removeAllRanges()
-
-    alert("Código copiado!")
-})    
-
-
-
-const btnCopiar3 = document.getElementById("copiarMnu003");
-
-const codigo3 = document.getElementById("codigo3")
-
-btnCopiar3.addEventListener("click", function(){
-    const range = document.createRange()
-    range.selectNode(codigo3)
-    window.getSelection().removeAllRanges()
-    window.getSelection().addRange(range)
-
-
-    document.execCommand("copy")
-
-    window.getSelection().removeAllRanges()
-
-    alert("Código copiado!")
-})    
-
-
-
-
-const btnCopiar4 = document.getElementById("copiarMnu004");
-
-const codigo4 = document.getElementById("codigo4")
-
-btnCopiar4.addEventListener("click", function(){
-    const range = document.createRange()
-    range.selectNode(codigo4)
-    window.getSelection().removeAllRanges()
-    window.getSelection().addRange(range)
-
-
-    document.execCommand("copy")
-
-    window.getSelection().removeAllRanges()
-
-    alert("Código copiado!")
-})    
-
-
-
-
-const btnCopiar5 = document.getElementById("copiarMnu005");
-
-const codigo5 = document.getElementById("codigo5")
-
-btnCopiar5.addEventListener("click", function(){
-    const range = document.createRange()
-    range.selectNode(codigo5)
-    window.getSelection().removeAllRanges()
-    window.getSelection().addRange(range)
-
-
-    document.execCommand("copy")
-
-    window.getSelection().removeAllRanges()
-
-    alert("Código copiado!")
-})    
-
-
-const btnCopiar6 = document.getElementById("copiarMnu006");
-
-const codigo6 = document.getElementById("codigo6")
-
-btnCopiar6.addEventListener("click", function(){
-    const range = document.createRange()
-    range.selectNode(codigo6)
-    window.getSelection().removeAllRanges()
-    window.getSelection().addRange(range)
-
-
-    document.execCommand("copy")
-
-    window.getSelection().removeAllRanges()
-
-    alert("Código copiado!")
-})    
-
-
-
-const btnCopiar7 = document.getElementById("copiarMnu007");
-
-const codigo7 = document.getElementById("codigo7")
-
-btnCopiar7.addEventListener("click", function(){
-    const range = document.createRange()
-    range.selectNode(codigo7)
-    window.getSelection().removeAllRanges()
-    window.getSelection().addRange(range)
-
-
-    document.execCommand("copy")
-
-    window.getSelection().removeAllRanges()
-
-    alert("Código copiado!")
-})    
-
-
-const btnCopiar8 = document.getElementById("copiarMnu008");
-
-const codigo8 = document.getElementById("codigo8")
-
-btnCopiar8.addEventListener("click", function(){
-    const range = document.createRange()
-    range.selectNode(codigo8)
-    window.getSelection().removeAllRanges()
-    window.getSelection().addRange(range)
-
-
-    document.execCommand("copy")
-
-    window.getSelection().removeAllRanges()
-
-    alert("Código copiado!")
-})    
-
-
-
-const btnCopiar9 = document.getElementById("copiarMnu009");
-
-const codigo9 = document.getElementById("codigo9")
-
-btnCopiar9.addEventListener("click", function(){
-    const range = document.createRange()
-    range.selectNode(codigo9)
-    window.getSelection().removeAllRanges()
-    window.getSelection().addRange(range)
-
-
-    document.execCommand("copy")
-
-    window.getSelection().removeAllRanges()
-
-    alert("Código copiado!")
-})    
-
-
-
-
-const btnCopiar10 = document.getElementById("copiarMnu010");
-
-const codigo10 = document.getElementById("codigo10")
-
-btnCopiar10.addEventListener("click", function(){
-    const range = document.createRange()
-    range.selectNode(codigo10)
-    window.getSelection().removeAllRanges()
-    window.getSelection().addRange(range)
-
-
-    document.execCommand("copy")
-
-    window.getSelection().removeAllRanges()
-
-    alert("Código copiado!")
-})    
-
-
-
-const btnCopiar11 = document.getElementById("copiarMnu011");
-
-const codigo11 = document.getElementById("codigo11")
-
-btnCopiar11.addEventListener("click", function(){
-    const range = document.createRange()
-    range.selectNode(codigo11)
-    window.getSelection().removeAllRanges()
-    window.getSelection().addRange(range)
-
-
-    document.execCommand("copy")
-
-    window.getSelection().removeAllRanges()
-
-    alert("Código copiado!")
-})    
+function copiarTexto001() {
+    var codigos = document.getElementsByClassName("codigo001");
+    if (codigos.length > 0) {
+        var codigo = codigos[0];
+        var texto = codigo.innerText;
+
+        navigator.clipboard.writeText(texto)
+            .then(function() {
+                Swal.fire('Copiado Com Sucesso');
+            })
+            .catch(function(err) {
+                alert("Erro ao copiar o texto: " + err);
+            });
+    }
+}
+
+
+
+function copiarTexto002() {
+    var codigos = document.getElementsByClassName("codigo002");
+    if (codigos.length > 0) {
+        var codigo = codigos[0];
+        var texto = codigo.innerText;
+
+        navigator.clipboard.writeText(texto)
+            .then(function() {
+                Swal.fire('Copiado Com Sucesso');
+            })
+            .catch(function(err) {
+                alert("Erro ao copiar o texto: " + err);
+            });
+    }
+}
+
+
+
+function copiarTexto003() {
+    var codigos = document.getElementsByClassName("codigo003");
+    if (codigos.length > 0) {
+        var codigo = codigos[0];
+        var texto = codigo.innerText;
+
+        navigator.clipboard.writeText(texto)
+            .then(function() {
+                Swal.fire('Copiado Com Sucesso');
+            })
+            .catch(function(err) {
+                alert("Erro ao copiar o texto: " + err);
+            });
+    }
+}
+
+function copiarTexto004() {
+    var codigos = document.getElementsByClassName("codigo004");
+    if (codigos.length > 0) {
+        var codigo = codigos[0];
+        var texto = codigo.innerText;
+
+        navigator.clipboard.writeText(texto)
+            .then(function() {
+                Swal.fire('Copiado Com Sucesso');
+            })
+            .catch(function(err) {
+                alert("Erro ao copiar o texto: " + err);
+            });
+    }
+}
+
+
+function copiarTexto005() {
+    var codigos = document.getElementsByClassName("codigo005");
+    if (codigos.length > 0) {
+        var codigo = codigos[0];
+        var texto = codigo.innerText;
+
+        navigator.clipboard.writeText(texto)
+            .then(function() {
+                Swal.fire('Copiado Com Sucesso');
+            })
+            .catch(function(err) {
+                alert("Erro ao copiar o texto: " + err);
+            });
+    }
+}
+
+
+function copiarTexto006() {
+    var codigos = document.getElementsByClassName("codigo006");
+    if (codigos.length > 0) {
+        var codigo = codigos[0];
+        var texto = codigo.innerText;
+
+        navigator.clipboard.writeText(texto)
+            .then(function() {
+                Swal.fire('Copiado Com Sucesso');
+            })
+            .catch(function(err) {
+                alert("Erro ao copiar o texto: " + err);
+            });
+    }
+}
+
+function copiarTexto007() {
+    var codigos = document.getElementsByClassName("codigo007");
+    if (codigos.length > 0) {
+        var codigo = codigos[0];
+        var texto = codigo.innerText;
+
+        navigator.clipboard.writeText(texto)
+            .then(function() {
+                Swal.fire('Copiado Com Sucesso');
+            })
+            .catch(function(err) {
+                alert("Erro ao copiar o texto: " + err);
+            });
+    }
+}
+
+function copiarTexto008() {
+    var codigos = document.getElementsByClassName("codigo008");
+    if (codigos.length > 0) {
+        var codigo = codigos[0];
+        var texto = codigo.innerText;
+
+        navigator.clipboard.writeText(texto)
+            .then(function() {
+                Swal.fire('Copiado Com Sucesso');
+            })
+            .catch(function(err) {
+                alert("Erro ao copiar o texto: " + err);
+            });
+    }
+}
+
+function copiarTexto009() {
+    var codigos = document.getElementsByClassName("codigo009");
+    if (codigos.length > 0) {
+        var codigo = codigos[0];
+        var texto = codigo.innerText;
+
+        navigator.clipboard.writeText(texto)
+            .then(function() {
+                Swal.fire('Copiado Com Sucesso');
+            })
+            .catch(function(err) {
+                alert("Erro ao copiar o texto: " + err);
+            });
+    }
+}
+
+function copiarTexto010() {
+    var codigos = document.getElementsByClassName("codigo010");
+    if (codigos.length > 0) {
+        var codigo = codigos[0];
+        var texto = codigo.innerText;
+
+        navigator.clipboard.writeText(texto)
+            .then(function() {
+                Swal.fire('Copiado Com Sucesso');
+            })
+            .catch(function(err) {
+                alert("Erro ao copiar o texto: " + err);
+            });
+    }
+}
+
+
+function copiarTexto011() {
+    var codigos = document.getElementsByClassName("codigo011");
+    if (codigos.length > 0) {
+        var codigo = codigos[0];
+        var texto = codigo.innerText;
+
+        navigator.clipboard.writeText(texto)
+            .then(function() {
+                Swal.fire('Copiado Com Sucesso');
+            })
+            .catch(function(err) {
+                alert("Erro ao copiar o texto: " + err);
+            });
+    }
+}
+
+function copiarTexto012() {
+    var codigos = document.getElementsByClassName("codigo012");
+    if (codigos.length > 0) {
+        var codigo = codigos[0];
+        var texto = codigo.innerText;
+
+        navigator.clipboard.writeText(texto)
+            .then(function() {
+                Swal.fire('Copiado Com Sucesso');
+            })
+            .catch(function(err) {
+                alert("Erro ao copiar o texto: " + err);
+            });
+    }
+}
+
+
+function copiarTexto013() {
+    var codigos = document.getElementsByClassName("codigo013");
+    if (codigos.length > 0) {
+        var codigo = codigos[0];
+        var texto = codigo.innerText;
+
+        navigator.clipboard.writeText(texto)
+            .then(function() {
+                Swal.fire('Copiado Com Sucesso');
+            })
+            .catch(function(err) {
+                alert("Erro ao copiar o texto: " + err);
+            });
+    }
+}
+
+
+function copiarTexto014() {
+    var codigos = document.getElementsByClassName("codigo014");
+    if (codigos.length > 0) {
+        var codigo = codigos[0];
+        var texto = codigo.innerText;
+
+        navigator.clipboard.writeText(texto)
+            .then(function() {
+                Swal.fire('Copiado Com Sucesso');
+            })
+            .catch(function(err) {
+                alert("Erro ao copiar o texto: " + err);
+            });
+    }
+}
+
+function copiarTexto015() {
+    var codigos = document.getElementsByClassName("codigo015");
+    if (codigos.length > 0) {
+        var codigo = codigos[0];
+        var texto = codigo.innerText;
+
+        navigator.clipboard.writeText(texto)
+            .then(function() {
+                Swal.fire('Copiado Com Sucesso');
+            })
+            .catch(function(err) {
+                alert("Erro ao copiar o texto: " + err);
+            });
+    }
+}
+
+
+
+function copiarTexto016() {
+    var codigos = document.getElementsByClassName("codigo016");
+    if (codigos.length > 0) {
+        var codigo = codigos[0];
+        var texto = codigo.innerText;
+
+        navigator.clipboard.writeText(texto)
+            .then(function() {
+                Swal.fire('Copiado Com Sucesso');
+            })
+            .catch(function(err) {
+                alert("Erro ao copiar o texto: " + err);
+            });
+    }
+}
